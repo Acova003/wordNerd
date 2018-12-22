bab1f2780ee0fc3d64ca431c487f359194fa826e
Amee <ameecova@yahoo.com>
2018-12-22 13:52:50 -0800

    add logo

--- app/controllers/song_info_controller.rb
+++ app/controllers/song_info_controller.rb
@@ -4,6 +4,7 @@ class SongInfoController < ApplicationController
     doc = Nokogiri::HTML(open('http://creativitygames.net/random-word-generator/randomwords/2'))
     @word_1 = doc.css('.randomword_1').text
     @word_2 = doc.css('.randomword_2').text
+    @doc =require 'open-uri';open('http://creativitygames.net/random-word-generator/randomwords/2').read
     render template: 'pages/home'
     # require 'openssl'
     # doc = Nokogiri::HTML(open('https://www.abokifx.com/bdc_rate', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
